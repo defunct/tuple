3ce5e9013f649ac63e65e98c47f1c15a8a7c5a32
Alan Gutierrez <alan@blogometer.com>
2010-05-06 02:49:02 -0500

    Updated Mix build for new project language.

--- src/mix/java/com/mallardsoft/tuple/mix/TupleProject.java
+++ src/mix/java/com/mallardsoft/tuple/mix/TupleProject.java
@@ -1,6 +1,5 @@
 package com.mallardsoft.tuple.mix;
 
-import com.goodworkalan.go.go.Artifact;
 import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
@@ -10,10 +9,10 @@ public class TupleProject extends ProjectModule {
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
-                .produces(new Artifact("com.mallardsoft/tuple/2.0"))
+                .produces("com.mallardsoft/tuple/2.0")
                 .test()
                     .depends()
-                        .artifact(new Artifact("junit/junit/4.4"))
+                        .include("junit/junit/4.4")
                         .end()
                     .end()
                 .end()
