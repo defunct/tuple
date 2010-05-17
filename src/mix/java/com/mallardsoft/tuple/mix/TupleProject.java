f1630e497970438296968adfa6171f99ba6d4724
Alan Gutierrez <alan@blogometer.com>
2010-05-16 23:14:18 -0500

    Mix ProjectModule is now an interface.

--- src/mix/java/com/mallardsoft/tuple/mix/TupleProject.java
+++ src/mix/java/com/mallardsoft/tuple/mix/TupleProject.java
@@ -4,16 +4,24 @@ import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
 
-public class TupleProject extends ProjectModule {
-    @Override
+/**
+ * Builds the project definition for Tuple.
+ *
+ * @author Alan Gutierrez
+ */
+public class TupleProject implements ProjectModule {
+    /**
+     * Build the project definition for Tuple.
+     *
+     * @param builder
+     *          The project builder.
+     */
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
                 .produces("com.mallardsoft/tuple/2.0")
-                .test()
-                    .depends()
-                        .include("junit/junit/4.4")
-                        .end()
+                .depends()
+                    .development("junit/junit/4.4")
                     .end()
                 .end()
             .end();
